603994681106a87018821f2b640aa7b4adedf4be
Fernanda Aparecida <ferreira.aparecidaf@gmail.com>
2017-11-17 16:53:06 -0200

    Java 8: início da Aula 02

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,51 +1,18 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
-		palavras.add("ediora casa do código");
+		palavras.add("editora casa do código");
 		palavras.add("caelum");
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		
-		palavras.sort(comparador);
-		
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+		palavras.forEach(s -> System.out.println(s));
 	}
-	
-}
 
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);		
-	}
-	
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		
-		if(s1.length() < s2.length()) {
-			return -1;
-		}
-		
-		if(s1.length() > s2.length()) {
-			return 1;
-		}		
-		
-		return 0;
-	}
-	
 }
