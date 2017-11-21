f90cca174be8efe71b35d4902f46c7f0140d2dc5
Fernanda Aparecida <ferreira.aparecidaf@gmail.com>
2017-11-21 17:35:11 -0200

    Java 8: Aula 05

--- Java8/src/ExemploCursos.java
+++ Java8/src/ExemploCursos.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 class Curso {
 
@@ -35,10 +39,10 @@ public class ExemploCursos {
 		cursos.sort( Comparator.comparingInt( Curso::getAlunos ) );
 		
 		//cursos.forEach( c -> System.out.println(c.getNome()));
+	
 		
-		cursos.stream()
-			.map(Curso::getNome);
-			
+		//cursos.forEach( c -> System.out.println(c.getNome()) );
+		
 		
 	}
 	
