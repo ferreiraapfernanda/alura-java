fc382d4f14aab9a6be6e5d98ffd9f2f5853f1a29
Fernanda Aparecida <ferreira.aparecidaf@gmail.com>
2017-11-23 08:57:08 -0200

    Java Collections: Aula 04

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -6,11 +6,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
-	
+
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
-		
+
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
 		this.instrutor = instrutor;
@@ -19,16 +19,25 @@ public class Curso {
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public String getInstrutor() {
 		return instrutor;
 	}
-	
+
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
 	}
 
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
+	}
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + nome + ", tempo total: " + this.getTempoTotal() + ", aulas: " + this.aulas + "]";
 	}
 }
---
+++ gerenciador-de-cursos/src/br/com/alura/TestaCurso2.java
@@ -0,0 +1,29 @@
+package br.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TestaCurso2 {
+
+	public static void main(String[] args) {
+		
+Curso javaColecoes = new Curso("Dominando as coleções da Java", "Paullo Silveira");
+		
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criado uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
+		
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
+		
+		List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+		
+		Collections.sort(aulas);
+		
+		System.out.println(aulas);
+		System.out.println(javaColecoes.getTempoTotal());
+		
+		System.out.println(javaColecoes);
+	}
+}
