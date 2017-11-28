3ecf3ac1c4308ce669eecf314cbd9b02dd6d929a
Fernanda Aparecida <ferreira.aparecidaf@gmail.com>
2017-11-28 17:33:04 -0200

    Java Collections: Aula 09

---
+++ gerenciador-de-cursos/src/br/com/alura/QualColecaoUsar.java
@@ -0,0 +1,13 @@
+package br.com.alura;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class QualColecaoUsar {
+	
+	public static void main(String[] args) {
+		Collection<Aluno> alunos =  new HashSet<>();
+	
+	}
+
+}
