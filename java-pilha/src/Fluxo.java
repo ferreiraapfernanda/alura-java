fae17ec794fbfe68c2d6fa72d34673ae419191f6
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-03 15:21:17 -0300

    Java parte 4 - Aula 03

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -3,30 +3,27 @@ public class Fluxo {
 
 	public static void main(String[] args) {
 		System.out.println("Inicio do main");
-		metodo1();
+		try{
+			metodo1();
+		} catch (ArithmeticException | NullPointerException e) {
+			String msg = e.getMessage();
+			System.out.println("Expection " + msg);
+			e.printStackTrace();
+		}
 		System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 		System.out.println("Inicio do metodo1");
-		try {
-			metodo2();
-		} catch (ArithmeticException | NullPointerException e) {
-			String msg = e.getMessage();
-			System.out.println("Exception " + msg);
-			e.printStackTrace();
-		}
+		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
 	private static void metodo2() {
 		System.out.println("Inicio do metodo2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
-			int a = i / 0;
-			// Conta conta = null;
-			// conta.deposita();
-		}
-		System.out.println("Fim do metodo2");
+		
+		throw new ArithmeticException("deu errado");
+
+		// System.out.println("Fim do metodo2");
 	}
 }
---
+++ java-pilha/src/FluxoComTratamento.java
@@ -0,0 +1,32 @@
+
+public class FluxoComTratamento {
+
+	public static void main(String[] args) {
+		System.out.println("Inicio do main");
+		metodo1();
+		System.out.println("Fim do main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Inicio do metodo1");
+		try {
+			metodo2();
+		} catch (ArithmeticException | NullPointerException e) {
+			String msg = e.getMessage();
+			System.out.println("Exception " + msg);
+			e.printStackTrace();
+		}
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Inicio do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			int a = i / 0;
+			// Conta conta = null;
+			// conta.deposita();
+		}
+		System.out.println("Fim do metodo2");
+	}
+}
