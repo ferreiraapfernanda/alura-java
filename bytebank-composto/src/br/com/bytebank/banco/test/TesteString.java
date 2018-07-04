b069d06655dd34fefd411a6554b99be9def11c32
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-04 11:49:48 -0300

    Java parte 5 - Aula 05

---
+++ bytebank-composto/src/br/com/bytebank/banco/test/TesteString.java
@@ -0,0 +1,31 @@
+package br.com.bytebank.banco.test;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String nome = "Mario";
+		String outro = new String("ALura");
+		
+		String novo = nome.replace("A", "a");
+		
+		novo = nome.toLowerCase();
+		novo = nome.toUpperCase();
+		
+		System.out.println(novo);
+		
+		char c = nome.charAt(3);
+		System.out.println(c);
+		
+		int pos = nome.indexOf("rio");
+		System.out.println(pos);
+		
+		String sub = nome.substring(1);
+		System.out.println(sub);
+		
+		for(int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
+			
+		}
+	}
+}
