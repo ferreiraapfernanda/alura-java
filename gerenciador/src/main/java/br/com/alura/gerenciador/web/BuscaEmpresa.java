4c7dd50b268221b18968e8ea8e7df6c35d164376
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-05 17:40:59 -0300

    Servlets - Aula 01

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -0,0 +1,41 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+@WebServlet(urlPatterns = "/busca")
+public class BuscaEmpresa extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("Resultado da busca:<br/>");
+
+		String filtro = req.getParameter("filtro");
+		Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
+
+		writer.println("<ul>");
+		for (Empresa empresa : empresas) {
+			writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
+		}
+		writer.println("</ul>");
+
+		writer.println("</body>");
+		writer.println("</html>");
+
+	}
+
+}
