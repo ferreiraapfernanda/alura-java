fa5789a58cdfab70c975a57814e74bd6d9364d84
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-06 11:19:26 -0300

    Servlets - Aula 09

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,15 +1,12 @@
 package br.com.alura.gerenciador.web;
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import br.com.alura.gerenciador.Usuario;
 
 @WebServlet(urlPatterns = "/logout")
 public class Logout extends HttpServlet {
@@ -20,9 +17,7 @@ public class Logout extends HttpServlet {
 		req.getSession().setMaxInactiveInterval(10*60);
 		req.getSession().removeAttribute("usuario.logado");
 		//req.getSession().invalidate();
-		
-		PrintWriter writer = resp.getWriter();
-
-		writer.println("<html><body>Deslogado com sucesso</body></html>");
+		RequestDispatcher dispatcher =  req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+		dispatcher.forward(req, resp);
 	}
 }
