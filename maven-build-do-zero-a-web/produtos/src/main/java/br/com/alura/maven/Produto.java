1516863751c6585f603547a890b2d3c06fd8a99e
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-10 10:35:22 -0300

    Maven - Aula 02

--- maven-build-do-zero-a-web/produtos/src/main/java/br/com/alura/maven/Produto.java
+++ maven-build-do-zero-a-web/produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -2,4 +2,13 @@ package br.com.alura.maven;
 
 public class Produto {
 
+	private final String nome;
+	private final double preco;
+
+	public Produto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+
 }
