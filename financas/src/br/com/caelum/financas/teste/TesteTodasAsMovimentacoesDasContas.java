5043f040a4e650404395486ba49f161c18877db2
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-16 10:14:54 -0300

    Java e JPA: Aula 07

--- financas/src/br/com/caelum/financas/teste/TesteTodasAsMovimentacoesDasContas.java
+++ financas/src/br/com/caelum/financas/teste/TesteTodasAsMovimentacoesDasContas.java
@@ -14,7 +14,7 @@ public class TesteTodasAsMovimentacoesDasContas {
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
-		String jpql = "select c from Conta c join fetch c.movimentacoes";
+		String jpql = "select distinct c from Conta c left join fetch c.movimentacoes";
 		Query query = em.createQuery(jpql);
 
 		List<Conta> todasAsContas = query.getResultList();
