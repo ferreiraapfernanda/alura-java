23effdf18776afa55a0052ac7e6532414217b9d6
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-27 16:23:08 -0300

    EJB - Aula 05

--- livraria/src/br/com/caelum/livraria/bean/AutorService.java
+++ livraria/src/br/com/caelum/livraria/bean/AutorService.java
@@ -3,8 +3,6 @@ package br.com.caelum.livraria.bean;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import br.com.caelum.livraria.dao.AutorDao;
@@ -16,13 +14,10 @@ public class AutorService {
 	@Inject
 	AutorDao dao;
 
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public void adiciona(Autor autor) {
 		// Mais regras de negócio
-
+		
 		dao.salva(autor);
-
-		throw new RuntimeException("[ERRO] Erro lançado para testar o rollback da transação.");
 	}
 
 	public List<Autor> todosAutores() {
--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -8,41 +8,32 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.UserTransaction;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
-@TransactionManagement(TransactionManagementType.BEAN)
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class AutorDao {
 
 	@PersistenceContext
 	private EntityManager manager;
-
-	@Inject
-	UserTransaction tx;
 
 	@PostConstruct
 	void aposCriacao() {
 		System.out.println("[INFO] AutorDao foi criado.");
 	}
 
-	 @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public void salva(Autor autor) {
 		System.out.println("[INFO] Salvando o Autor " + autor.getNome());
 
-		try {
-			tx.begin();
-			manager.persist(autor);
-			tx.commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+		manager.persist(autor);
+		
 		System.out.println("[INFO] Salvou o Autor " + autor.getNome());
+		
+		throw new RuntimeException("Serviço externo deu erro!");
 	}
 
 	public List<Autor> todosAutores() {
