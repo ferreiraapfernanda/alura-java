a8189f48349d3daafc9806266ee95003ec8dcf44
Fernanda Aparecida <fernanda@amee.com.br>
2018-07-30 15:50:12 -0300

    Java e JSF I - Aula 04

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,14 +1,20 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	private Integer autorId;
 
 	public Livro getLivro() {
 		return livro;
@@ -22,6 +28,33 @@ public class LivroBean {
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
 	}
 
 }
