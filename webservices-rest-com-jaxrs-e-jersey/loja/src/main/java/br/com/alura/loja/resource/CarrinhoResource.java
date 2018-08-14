106d488f4bb6d5b53a5b1705d8336dc336ba73bc
Fernanda Aparecida Ferreira <fernanda@amee.com.br>
2018-08-14 16:33:33 -0300

    JAX-RS e Jersey - Aula 11

--- webservices-rest-com-jaxrs-e-jersey/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ webservices-rest-com-jaxrs-e-jersey/loja/src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -47,4 +49,17 @@ public class CarrinhoResource {
 
 		return Response.ok().build();
 	}
+
+	@Path("{id}/produtos/{produtoId}/quantidade")
+	@PUT
+	public Response alteraProduto(@PathParam("id") long id, @PathParam("produtId") long produtoId, String conteudo) {
+
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.trocaQuantidade(produto);
+
+		return Response.ok().build();
+
+	}
+
 }
